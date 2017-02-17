d36adf2a9f8ae9565c4344d304c0ca3494eeb156
alexapostol <alexandruapostol93i@gmail.com>
2017-02-17 12:53:40 +0200

    Tank created, Look position, Hitposition, blueprint callable, subobjects

---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" // Must be the last include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+private:
+	ATank* GetControlledTank() const;
+	virtual void Tick(float DeltaTime) override;
+	virtual void BeginPlay() override;
+
+	// start the tank moving the barrel so that would it where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+		float CrosshairYLocation = 0.33333;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000;
+};
